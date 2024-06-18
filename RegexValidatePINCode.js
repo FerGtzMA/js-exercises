9dec286d9514cf8a4a1b86116241febc28087470
Fernando Gutierrez <fernando.gutierrezsn@uanl.edu.mx>
2024-06-18 11:52:27 -0600

    I: PIN Verificator

---
+++ RegexValidatePINCode.js
@@ -0,0 +1,28 @@
+/* 
+ATM machines allow 4 or 6 digit PIN codes and PIN codes 
+cannot contain anything but exactly 4 digits or exactly 
+6 digits.
+
+If the function is passed a valid PIN string, return true, 
+else return false.
+
+Examples (Input --> Output)
+"1234"   -->  true
+"12345"  -->  false
+"a234"   -->  false
+*/
+
+function validatePIN (pin) {
+    return /^(\d{4}|\d{6})$/.test(pin);
+}
+
+console.log(validatePIN("123t56"));
+
+/* 
+En esta expresión regular:
+-   ^ indica el inicio de la cadena.
+-   \d{4} verifica exactamente 4 dígitos consecutivos.
+-   | es el operador "o".
+-   \d{6} verifica exactamente 6 dígitos consecutivos.
+-   $ indica el final de la cadena.
+*/
